54fb0b3404b88d0aeb5be5636bc004459250622c
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2021-09-24 17:37:32 +0530

    test

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import ScreenArea from './components/ScreenArea'
 import Color from './config/Colors'
 import Navigation from './navigation/MainStackNavigation'
 import { NavigationContainer } from '@react-navigation/native'
+
 export default function App() {
   return (
     <ScreenArea screenColor={Color.lightGrey}>
